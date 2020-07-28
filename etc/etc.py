1d67575648b7590bd098eaa95d03dde321099bfe
Tim Lister <tlister@lcogt.net>
2020-07-28 10:11:37 -0700

    Add basic initialization of components

--- etc/etc.py
+++ etc/etc.py
@@ -13,4 +13,16 @@ class ETC(object):
 
     def __init__(self, components=None, config_file=None):
         PRESET_MODELS = toml.loads(pkg_resources.read_text(data, "FTN_FLOYDS.toml"))
-        self.components = []
+        self.components = components if components is not None else []
+        if config_file is None and len(self.components) == 0:
+            component_config = PRESET_MODELS
+        elif config_file is not None:
+            component_config = toml.load(config_file)
+        else:
+            component_config = {}
+        if len(component_config) > 0:
+            self.create_components_from_config(component_config)
+
+    def create_components_from_config(self, config):
+        for component, component_config in config.items():
+            self.components.append(component)
--- etc/tests/test_etc.py
+++ etc/tests/test_etc.py
@@ -2,7 +2,12 @@ import pytest
 from etc import etc
 
 
-def test_initialize(capsys):
+def test_initialize():
     test_etc = etc.ETC()
 
     assert ['site', 'telescope', 'instrument'] == test_etc.components
+
+def test_initialize_specified_components():
+    test_etc = etc.ETC(components=['sky', 'earth', 'underground cavern'])
+
+    assert ['sky', 'earth', 'underground cavern'] == test_etc.components
