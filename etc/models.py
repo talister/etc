f3919e118be439d50c80bc250c69f1d7d5579568
Tim Lister <tlister@lcogt.net>
2020-07-29 17:15:08 -0700

    Attempt to make Site inherit from and behave as a SpectralElement (PARTLY BROKEN)

--- etc/models.py
+++ etc/models.py
@@ -1,13 +1,37 @@
-from astropy import units as u
+import os
 
-class Site:
+import numpy as np
+from astropy import units as u
+from synphot import units, SourceSpectrum, SpectralElement, specio
+from synphot.spectrum import BaseUnitlessSpectrum, Empirical1D, ConstFlux1D
+
+class Site(SpectralElement):
     """Model for a site location and the atmosphere above it"""
 
-    def __init__(self, name=None, altitude=None, latitude= None, longitude=None, **kwargs):
+    def __init__(self, name=None, altitude=None, latitude= None, longitude=None, *args, **kwargs):
         self.name = name if name is not None else "Undefined"
-        self.altitude = altitude * u.m
+        self.altitude = altitude * u.m if altitude is not None else altitude
         self.latitude = latitude
         self.longitude = longitude
+        self.meta = {}
+        if 'transmission' in kwargs:
+            modelclass = Empirical1D
+            try:
+                transmission = float(kwargs['transmission'])
+                print("Is float")
+                wavelengths = np.arange(300, 1501, 200) * u.nm
+                throughput = len(wavelengths) * [transmission,]
+                header = {}
+            except ValueError:
+                sky_file = os.path.expandvars(kwargs['transmission'])
+                print("Is filename", sky_file)
+                header, wavelengths, throughput = specio.read_spec(sky_file, wave_col='lam', flux_col='trans', wave_unit=u.nm,flux_unit=u.dimensionless_unscaled)
+            super().__init__(modelclass, points=wavelengths, lookup_table=throughput, keep_neg=True, meta={'header': header})
+            print("model=",self._model)
+        # else:
+            # wavelengths, lookup_table = self._get_arrays(self.waveset)
+            # print("model=",self._model)
+            # super().__init__(self._model, points=wavelengths, lookup_table=lookup_table, *args, **kwargs)
 
     def __repr__(self):
         return "[{}({})]".format(self.__class__.__name__, self.name)
