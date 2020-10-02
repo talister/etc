3dc1f2132836a0412b647c480b68600b67a36ac7
Tim Lister <tlister@lcogt.net>
2020-10-02 16:56:38 -0700

    Add ETC.ccd_snr() and ETC.exptime_from_ccd_snr() routines to calculate SNR and exptime to reach a given SNR

--- etc/etc.py
+++ etc/etc.py
@@ -11,6 +11,7 @@ except ImportError:
     # Try backported to PY<37 `importlib_resources`.
     import importlib_resources as pkg_resources
 
+import numpy as np
 from astropy import units as u
 from synphot import SourceSpectrum, SpectralElement, units
 from synphot.models import Empirical1D
@@ -28,7 +29,7 @@ class ETC(object):
     _vega = SourceSpectrum.from_file(os.path.expandvars(conf.vega_file))
     _V_band = SpectralElement.from_filter('johnson_v')
 
-    def __init__(self, components=None, config_file=None):
+    def __init__(self, config_file=None, components=None):
         PRESET_MODELS = toml.loads(pkg_resources.read_text(data, "FTN_FLOYDS.toml"))
         self.components = components if components is not None else []
         if config_file is None and len(self.components) == 0:
@@ -66,8 +67,7 @@ class ETC(object):
         insts = [x for x in self.components if isinstance(x, Instrument)]
         return insts[0] if len(insts) == 1 else None
 
-    def exptime_from_ccd_snr(self, snr, V_mag, filtername):
-
+    def photons_from_source(self, V_mag, filtername):
         sun = self._vega.normalize(V_mag * units.VEGAMAG, self._V_band, vegaspec=self._vega)
 
         source_spec = sun
@@ -80,19 +80,242 @@ class ETC(object):
         synphot_obs = Observation(source_spec, spec_elements, force='taper')
 
         countrate = synphot_obs.countrate(area=self.telescope.area)
+
+        return countrate
+
+    def ccd_snr(self, exp_time, V_mag, filtername, npix=1 * u.pixel,
+                         n_background=np.inf * u.pixel,
+                         background_rate=0 * (u.ph / u.pixel / u.s),
+                         sky_mag=None,
+                         darkcurrent_rate=0 * (u.ph / u.pixel / u.s)):
+        """Calculate the SNR in a given exposure time <exp_time> for a given <V_mag>
+        """
+
+        try:
+            exp_time = exp_time.to(u.s)
+        except AttributeError:
+            exp_time = exp_time * u.s
+
+        # Get countrate per second for this magnitude in the filter
+        countrate = self.photons_from_source(V_mag, filtername)
+
+        # define countrate to be in photons/sec, which can be treated as the
+        # same as (photo)electrons for CCDs but are not technically convertible in
+        # astropy.units:
+        countrate = countrate.value * (u.photon / u.s)
+
+        print("Counts/s=", countrate)
+
+        readnoise = self.instrument.ccd_readnoise.value * (u.photon/u.pixel)
+        readnoise_sq = readnoise.value**2 * (u.photon/u.pixel)
+        gain_err = self._get_shotnoise(self.instrument.ccd_gain * self.instrument.adc_error)
+        print("Readnoise=", readnoise, readnoise_sq, gain_err)
+        if sky_mag is not None:
+            # Compute countrate from given sky magnitude.
+            # XXX need to refactor photons_from_source() to do this also
+            sky_filtername = filtername
+            if '::' in filtername:
+                sky_filtername = filtername.split('::')[1]
+            sky = self.site.sky_spectrum(sky_filtername)
+            print("  Original sky=" ,sky(sky.avgwave()))
+            sky2 = sky.normalize(sky_mag*units.VEGAMAG, self._V_band, vegaspec=self._vega)
+            print("Normalized sky=", sky2(sky2.avgwave()), sky(sky.avgwave())*10**(-sky_mag/2.5))
+            self._create_combined()
+            waves, thru = self.combined._get_arrays(None)
+            obs_filter = self.instrument.filterset[filtername]
+            filter_waves, filter_trans = obs_filter._get_arrays(waves)
+            print('thruput=', thru.max(), self.telescope.area.to(units.AREA)*thru.max())
+            spec_elements = SpectralElement(Empirical1D, points=filter_waves, lookup_table = thru * filter_trans)
+
+            # get the synphot observation object
+            sky_obs = Observation(sky2, spec_elements, force='taper')
+
+            sky_countrate = sky_obs.countrate(area=self.telescope.area)
+            # Actually a count rate per arcsec^2 as the original magnitude is
+            # also per sq. arcsec. Also set to photons/s to match signal from object
+            sky_countrate = sky_countrate.value * (u.photon/u.s/u.arcsec**2)
+            print("Sky (photons/AA/arcsec^2)=", sky_countrate, sky_countrate/obs_filter.equivwidth())
+            sky_per_pixel = sky_countrate * self.instrument.ccd_pixscale * self.instrument.ccd_pixscale
+            sky_per_pixel /= u.pixel
+            print("Sky (photons/pixel)=", sky_per_pixel)
+            background_rate = sky_per_pixel
+
+        fwhm = self.instrument.fwhm
+        pixel_scale = self.instrument.ccd_pixscale
+        if npix == 1*u.pixel:
+            # Calculate new value based on area of FWHM and pixelscale
+            # IRAF `ccdtime` uses this definition
+    #        npix = 1.4*(fwhm / pixel_scale)**2
+            npix = np.pi*(fwhm / pixel_scale)**2
+            npix = int(round(npix.value)) * u.pixel
+
+        print("npix=", npix)
+        sobj2 = countrate * exp_time
+        sky_countrate = sky_countrate * exp_time/obs_filter.equivwidth()
+        dark_count = darkcurrent_rate * exp_time
+        noise_ccd = np.sqrt(npix * (dark_count + readnoise_sq))
+        print("Noise(Star)=", np.sqrt(sobj2))
+        print("Noise( CCD)=", noise_ccd)
+        ssky2 = background_rate * exp_time
+        peak = sobj2/1.14/fwhm/fwhm*pixel_scale*pixel_scale
+
+
+        print('Detected photons            from object =', sobj2, \
+             ' (max',peak,' /pixel)')
+        print('Detected photons/A/arcsec^2 from sky    =', sky_countrate)
+        print('Detected photons/pixel      from sky    =', ssky2)
+        snr = self._compute_snr(sobj2, ssky2, npix, dark_count, readnoise_sq)
+        print('Signal-to-noise                         =', snr.value)
+        return snr.value
+
+    def _compute_snr(self, sobj2, ssky2, npix, dark_count, readnoise_sq):
+        """Compute the signal-to-noise via the CCD equation
+        Ref: Howell (2006), page 76
+        """
+        snr = sobj2 / np.sqrt(sobj2+npix*(ssky2+dark_count+readnoise_sq))
+
+        return snr
+
+    def exptime_from_ccd_snr(self, snr, V_mag, filtername, npix=1 * u.pixel,
+                         n_background=np.inf * u.pixel,
+                         background_rate=0 * (u.ct / u.pixel / u.s),
+                         sky_mag=None,
+                         darkcurrent_rate=0 * (u.ct / u.pixel / u.s)):
+
+        countrate = self.photons_from_source(V_mag, filtername)
+
+        # make sure the gain is in the correct units
+        gain = self.instrument.ccd_gain
+        if gain.unit in (u.electron / u.adu, u.photon / u.adu):
+            gain = gain.value * (u.ct / u.adu)
+        elif gain.unit != u.ct / u.adu:
+            raise u.UnitsError('gain must have units of (either '
+                               'astropy.units.ct, astropy.units.electron, or '
+                               'astropy.units.photon) / astropy.units.adu')
+
+        # get the countrate from the synphot observation object
+        countrate = countrate / gain
         print(countrate)
+        # define counts to be in ADU, which are not technically convertible in
+        # astropy.units:
+        countrate = countrate.value * (u.ct / u.s)
+
+        # necessary for units to work in countrate calculation:
+        if not hasattr(snr, 'unit'):
+            snr = snr * np.sqrt(1 * u.ct)
+        readnoise = self._get_shotnoise(self.instrument.ccd_readnoise)
+        gain_err = self._get_shotnoise(self.instrument.ccd_gain * self.instrument.adc_error)
+        print(readnoise, gain_err)
+        if sky_mag is not None:
+            # Compute countrate from given sky magnitude.
+            # XXX need to refactor photons_from_source() to do this also
+            sky_filtername = filtername
+            if '::' in filtername:
+                sky_filtername = filtername.split('::')[1]
+            sky = self.site.sky_spectrum(sky_filtername)
+            print("  Original sky=" ,sky(sky.avgwave()))
+            sky2 = sky.normalize(sky_mag*units.VEGAMAG, self._V_band, vegaspec=self._vega)
+            print("Normalized sky=", sky2(sky2.avgwave()), sky(sky.avgwave())*10**(-sky_mag/2.5))
+            self._create_combined()
+            waves, thru = self.combined._get_arrays(None)
+            obs_filter = self.instrument.filterset[filtername]
+            filter_waves, filter_trans = obs_filter._get_arrays(waves)
+            signal_combined = self.combined/self.instrument.ccd_qe
+            signal_waves, signal_thru = signal_combined._get_arrays(None)
+            print('thruput (Atm*tel*instr)=', signal_thru.max())
+            print('thruput (Atm*tel*instr*CCD)=', thru.max(), self.telescope.area.to(units.AREA)*thru.max())
+            spec_elements = SpectralElement(Empirical1D, points=filter_waves, lookup_table = thru * filter_trans)
+
+            # get the synphot observation object
+            sky_obs = Observation(sky2, spec_elements, force='taper')
+
+            sky_countrate = sky_obs.countrate(area=self.telescope.area)
+            # Actually a count rate per arcsec^2 as the original magnitude is
+            # also per sq. arcsec
+            sky_countrate /= u.arcsec**2
+            print("Sky (photons/AA/arcsec^2)=", sky_countrate, sky_countrate/(1.0*860)*u.AA)
+            sky_per_pixel = sky_countrate * self.instrument.ccd_pixscale * self.instrument.ccd_pixscale
+            sky_per_pixel /= u.pixel
+            print("Sky (photons/pixel)=", sky_per_pixel)
+            background_rate = sky_per_pixel
+
+        npix = np.pi*(self.instrument.fwhm / self.instrument.ccd_pixscale)**2
+        npix *= u.pixel
+        print("npix=", npix)
+
+        # solve t with the quadratic equation (pg. 77 of Howell 2006)
+        A = countrate ** 2
+        B = (-1) * snr ** 2 * (countrate + npix * (background_rate +
+                                                   darkcurrent_rate))
+        C = (-1) * snr ** 2 * npix * readnoise ** 2
+        t = (-B + np.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+
+        if gain_err.value > 1 or np.isfinite(n_background.value):
+            from scipy.optimize import fsolve
+            # solve t numerically
+            t = fsolve(_t_with_small_errs, t, args=(background_rate,
+                                                    darkcurrent_rate,
+                                                    gain_err, readnoise, countrate,
+                                                    npix, n_background))
+            t = float(t) * u.s
+
+        return t
+
+
+    def _get_shotnoise(self, detector_property):
+        """
+        Returns the shot noise (i.e. non-Poissonion noise) in the correct
+        units. ``detector_property`` must be a Quantity.
+        """
+        # Ensure detector_property is in the correct units:
+        if detector_property.unit in (u.electron / u.pix,):
+            detector_property = (detector_property.value/self.instrument.ccd_gain.value) * (u.ct/u.pix)
+        detector_property = detector_property.to(u.ct / u.pixel)
+        return detector_property.value * np.sqrt(1 * (u.ct / u.pixel))
+
+
+    def _t_with_small_errs(self, t, background_rate, darkcurrent_rate, gain_err,
+                           readnoise, countrate, npix, n_background):
+        """
+        Returns the full expression for the exposure time including the
+        contribution to the noise from the background and the gain.
+        """
+        if not hasattr(t, 'unit'):
+            t = t * u.s
+
+        detector_noise = (background_rate * t + darkcurrent_rate * t +
+                          gain_err ** 2 + readnoise ** 2)
+        radicand = countrate * t + (npix * (1 + npix / n_background) *
+                                    detector_noise)
+
+        return countrate * t / np.sqrt(radicand)
 
     def _do_plot(self, waves, thru, filterlist=[], filterset=None, title='', left=300*u.nm, right=1200*u.nm, bottom=0.0, top=None):
         """Plot worker.
 
         Parameters
         ----------
-        waves, trans : `~astropy.units.quantity.Quantity`
+        waves, thru : `~astropy.units.quantity.Quantity`
             Wavelength and throughput to plot.
 
-        kwargs
-            See :func:`plot`.
+        filterset : list
+            List of filter bandpasses to plot
 
+        kwargs :
+
+        title : str
+            Plot title.
+
+        left, right : `None`, number or `Quantity`
+            Minimum and maximum wavelengths to plot.
+            If `None`, uses the whole range. If a number is given,
+            must be in Angstrom. If an `~astropy.units.quantity.Quantity`
+            is given, it will be converted tp the internal wave units.
+
+        bottom, top : `None` or number
+            Minimum and maximum flux/throughput to plot.
+            If `None`, uses the whole range. If a number is given,
+            it assumed to be in throughput (0..1).
         """
         try:
             import matplotlib.pyplot as plt
@@ -147,6 +370,20 @@ class ETC(object):
         plt.draw()
 
     def plot(self, filterlist=[], **kwargs):
+        """Plot combined system
+
+        Parameters
+        ----------
+        waves, thru : `~astropy.units.quantity.Quantity`
+            Wavelength and throughput to plot.
+
+        filterlist : list or str
+            Filter name(s) to plot
+
+        kwargs
+            See :func:`do_plot`.
+
+        """
         self._create_combined()
         waves, trans = self.combined._get_arrays(None)
         filterset = None
--- etc/tests/test_etc.py
+++ etc/tests/test_etc.py
@@ -1,7 +1,10 @@
 import pytest
 import os
+import warnings
 
 from astropy import units as u
+warnings.simplefilter("ignore", pytest.PytestUnknownMarkWarning)
+from astropy.tests.helper import assert_quantity_allclose
 
 from etc import etc
 
@@ -19,11 +22,11 @@ def test_initialize_specified_components():
 def test_initialize_config_file():
      test_config_file = os.path.abspath(os.path.join(__package__, 'etc', "tests", "data", "test1.toml"))
      test_etc = etc.ETC(config_file=test_config_file)
-     assert '<ETC [Site(BPL)]->[Telescope(BPL 1-m)]>' == repr(test_etc)
+     assert '<ETC [Site(BPL)]->[Telescope(BPL 1-m)]->[Instrument(Sinistro)]>' == repr(test_etc)
 
      assert test_etc.site.name == "BPL"
      assert test_etc.telescope.name == "BPL 1-m"
-     assert test_etc.instrument == None
+     assert test_etc.instrument.name == "Sinistro"
 
 def test_properties():
     test_etc = etc.ETC()
@@ -32,3 +35,42 @@ def test_properties():
     assert test_etc.site.altitude == 3065 * u.m
     assert test_etc.telescope.name == "FTN"
     assert test_etc.instrument.name == "FLOYDS"
+
+class TestComputeSNR:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        self.gain = 5.0*(u.electron/u.adu)
+        self.readnoise = 5
+        self.darkcurrent_rate = 22*(u.electron/u.pixel/u.hour)
+        self.exp_time = 300*u.s
+
+    def test_howell_example(self):
+        expected_snr = 342.07825332
+
+        test_etc = etc.ETC()
+
+        signal_obj = 24013 * self.gain.value
+        signal_sky = 620 * self.gain.value
+        npix = 1
+        signal_dark = self.darkcurrent_rate * self.exp_time
+        signal_dark = signal_dark.decompose().value
+        readnoise_sq = self.readnoise * self.readnoise
+
+        snr = test_etc._compute_snr(signal_obj, signal_sky, npix, signal_dark, readnoise_sq)
+
+        assert_quantity_allclose(expected_snr, snr, rtol=1e-5)
+
+class TestCCDSNR:
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+
+        self.test_config_file = os.path.abspath(os.path.join(__package__, 'etc', "tests", "data", "test1.toml"))
+
+    def test_LCO_1m_1s_V15(self):
+        expected_snr = 10.4210644
+
+        test_etc = etc.ETC(self.test_config_file)
+
+        snr = test_etc.ccd_snr(1, 15, 'V', sky_mag=21.8)
+
+        assert_quantity_allclose(expected_snr, snr, rtol=1e-5)
